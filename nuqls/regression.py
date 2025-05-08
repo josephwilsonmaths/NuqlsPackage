e03a185513f10d3f4bed72bf97eebb0294723596
joseph-wilson1 <111267558+joseph-wilson1@users.noreply.github.com>
2025-05-08 14:56:13 +1000

    single regression function

--- nuqls/regression.py
+++ nuqls/regression.py
@@ -1,6 +1,7 @@
 import torch
 from functorch import make_functional
 from torch.utils.data import DataLoader
+from torch.func import vmap, jacrev
 import tqdm
 import copy
 import nuqls.utils as utils
@@ -14,6 +15,91 @@ class regressionParallel(object):
         self.device = next(network.parameters()).device
 
     def train(self, train, train_bs=50, scale=1, S=10, epochs=100, lr=1e-3, mu=0.9, verbose=False):
+        if train_bs >= len(train):
+            return self.gd(
+                train=train,
+                scale=scale,
+                S=S,
+                epochs=epochs,
+                lr=lr,
+                mu=mu,
+                verbose=verbose
+            )
+        else:
+            return self.sgd(
+                train=train,
+                train_bs=train_bs,
+                scale=scale,
+                S=S,
+                epochs=epochs,
+                lr=lr,
+                mu=mu,
+                verbose=verbose
+            )
+
+    def gd(self, train, scale=1, S=10, epochs=100, lr=1e-3, mu=0.9, verbose=False):
+        fnet, params = make_functional(self.network)
+        theta_t = utils.flatten(params)
+        theta = torch.randn(size=(theta_t.shape[0],S),device=self.device)*scale + theta_t.unsqueeze(1)
+
+        ## Create loaders and get entire training set
+        train_loader_total = DataLoader(train,batch_size=len(train))
+        X,Y = next(iter(train_loader_total))
+
+        ## Compute jacobian of net, evaluated on training set
+        def fnet_single(params, x):
+            return fnet(params, x.unsqueeze(0)).squeeze(0)
+        J = vmap(jacrev(fnet_single), (None, 0))(params, X.to(self.device))
+        J = [j.detach().flatten(1) for j in J]
+        J = torch.cat(J,dim=1).detach()
+
+        # Set progress bar
+        if verbose:
+            pbar = tqdm.trange(epochs)
+        else:
+            pbar = range(epochs)
+        
+        ## Train S realisations of linearised networks
+        for epoch in pbar:
+            if self.device == torch.device('cuda'):
+                torch.cuda.reset_peak_memory_stats()
+
+            X, Y = X.to(self.device), Y.to(self.device).reshape(-1,1)
+            
+            f_nlin = self.network(X)
+            f_lin = (J.to(self.device) @ (theta - theta_t.unsqueeze(1)) + f_nlin).detach()
+            resid = f_lin - Y
+            grad = J.T.to(self.device) @ resid.to(self.device) / X.shape[0]
+
+            if epoch == 0:
+                bt = grad
+            else:
+                bt = mu*bt + grad
+
+            theta -= lr * bt
+
+            loss = torch.mean(torch.square(J @ (theta - theta_t.unsqueeze(1)) + f_nlin - Y)).max()
+
+            if verbose:
+                metrics = {'mean_loss': loss.item(),
+                   'resid_norm': torch.mean(torch.square(grad)).item()}
+                if self.device == torch.device('cuda'):
+                    metrics['gpu_mem'] = 1e-9*torch.cuda.max_memory_allocated()
+                else:
+                    metrics['gpu_mem'] = 0
+                pbar.set_postfix(metrics)
+
+        # Report maximum loss over S, and the mean gradient norm
+        max_l2_loss = torch.mean(torch.square(J @ (theta - theta_t.unsqueeze(1)) + f_nlin - Y)).item()
+        norm_resid = torch.mean(torch.square(J.T @ ( f_nlin + J @ (theta - theta_t.unsqueeze(1)) - Y))).item()
+
+        if verbose:
+            print('Posterior samples computed!')
+        self.theta = theta
+
+        return max_l2_loss, norm_resid
+
+    def sgd(self, train, train_bs=50, scale=1, S=10, epochs=100, lr=1e-3, mu=0.9, verbose=False):
         fnet, params = make_functional(self.network)
 
         num_p = sum(p.numel() for p in self.network.parameters() if p.requires_grad)
